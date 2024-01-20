67c17947b7b137acef0666e3c79a6d725615bd56
Andrey Novikov <novikovandrey1984@gmail.com>
2024-01-20 15:51:35 +0300

    added tables Role, UserRole

--- src/models/models.py
+++ src/models/models.py
@@ -1,11 +1,11 @@
 import uuid
 
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, types, ForeignKey
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import (DeclarativeBase,
                             Mapped,
-                            mapped_column)
-
+                            mapped_column,
+                            relationship)
 
 metadata = MetaData()
 
@@ -17,13 +17,44 @@ class Base(AsyncAttrs, DeclarativeBase):
 class User(Base):
     __tablename__ = 'users'
 
-    uuid = Mapped[uuid.UUID] = mapped_column(default=uuid.uuid4(),
-                                             primary_key=True)
-    login = Mapped[str] = mapped_column(nullable=False, unique=True)
-    email = Mapped[str] = mapped_column(nullable=False, unique=True)
-    password = Mapped[str] = mapped_column(nullable=False)
-    name = Mapped[str] = mapped_column(nullable=True)
-    surname = Mapped[str] = mapped_column(nullable=True)
+    uuid: Mapped[uuid.UUID] = mapped_column(types.UUID,
+                                            default=uuid.uuid4,
+                                            primary_key=True)
+    login: Mapped[str] = mapped_column(nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(nullable=True)
+    surname: Mapped[str] = mapped_column(nullable=True)
+    user_role: Mapped['UserRole'] = relationship(back_populates='users',
+                                                 cascade='all, delete',
+                                                 passive_deletes=True)
+
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    uuid: Mapped[uuid.UUID] = mapped_column(types.UUID,
+                                            default=uuid.uuid4,
+                                            primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False, unique=True)
+
+
+class UserRole(Base):
+    __tablename__ = 'users_roles'
+
+    uuid: Mapped[uuid.UUID] = mapped_column(types.UUID,
+                                            default=uuid.uuid4,
+                                            primary_key=True)
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('users.uuid'),
+                                               onupdate='CASCADE',
+                                               ondelete='CASCADE',
+                                               nullable=False)
+    role_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('roles.uuid'),
+                                               onupdate='CASCADE',
+                                               ondelete='CASCADE',
+                                               nullable=False)
+    user: Mapped['User'] = relationship(back_populates='users_roles')
+    role: Mapped['Role'] = relationship(back_populates='users_roles')
 
 
 
