44ddeaca783e1ea0d122968c9c7e5fa11f115650
kseniia <ksusha.karpova2011@yandex.ru>
2024-01-24 22:01:52 +0300

    add exceptions for crud

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -9,4 +9,7 @@ user_exists = HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Login 
 
 role_not_found = HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Role not found")
 role_already_exist_error = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Role already exists")
-user_not_found = HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+role_user_already_exist_error = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Role already exists")
+crud_not_found = HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Can`t create rules")
+
+server_error = HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Sorry...")
