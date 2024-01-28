f946fadd06988403856f9f89f1cc871ddce1bbc4
kseniia <ksusha.karpova2011@yandex.ru>
2024-01-28 10:29:51 +0300

    fix crud

--- src/models/models.py
+++ src/models/models.py
@@ -25,6 +25,7 @@ class User(Base):
     name: Mapped[str] = mapped_column(nullable=True)
     surname: Mapped[str] = mapped_column(nullable=True)
     is_active: Mapped[bool] = mapped_column(default=True)  # instead deleting user, change this field
+    is_superuser: Mapped[bool] = mapped_column(default=False)
     user_role: Mapped['UserRole'] = relationship(back_populates='user',
                                                  cascade='all, delete',
                                                  passive_deletes=True)
@@ -40,9 +41,13 @@ class Role(Base):
                                        default=uuid.uuid4,
                                        primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False, unique=True)
-    user_roles: Mapped[List['UserRole']] = relationship(back_populates="role",
-                                                        cascade="all, delete",
-                                                        passive_deletes=True, )
+    user_roles: Mapped['UserRole'] = relationship(back_populates="role",
+                                                  cascade="all, delete",
+                                                  passive_deletes=True, )
+
+    @staticmethod
+    def get_colums():
+        return ['uuid', 'name']
 
 
 class UserRole(Base):
